76005b0b3714ea18309eeab789a53a46ecdf7b7d
hix <canbedir100@gmail.com>
2024-08-18 21:26:17 +0300

    feat: add Choose Us section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import ChooseUs from "@/components/ChooseUs/ChooseUs";
 import Hero from "@/components/Hero/Hero";
 import Navbar from "@/components/Navbar/Navbar";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <div className="mt-10">
       <Navbar/>
       <Hero/>
+      <ChooseUs/>
     </div>
   );
 }
---
+++ components/ChooseUs/ChooseUs.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import React from "react";
+
+const ChooseUs = () => {
+  return (
+    <div className="shadow-primary shadow-lg p-5">
+      <h1 className="text-5xl font-bold text-center">Why Choose Us?</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 mt-10 gap-20">
+        <div className="flex flex-col justify-center gap-2">
+          <h1 className="text-3xl font-bold text-primary">
+            User-Friendly Design
+          </h1>
+          <p className="text-muted-foreground">
+            We pride ourselves on offering a user-friendly interface that makes
+            portfolio creation as seamless as possible. Whether you're
+            tech-savvy or a beginner, our intuitive design ensures that you can
+            easily navigate through the process. Our platform guides you
+            step-by-step, allowing you to focus on what truly matters-showcasing
+            your talents and achievements in the best possible light. No
+            complicated settings or unnecessary distractions, just a clean,
+            efficient tool that helps you build a portfolio that stands out.
+          </p>
+        </div>
+        <div className="flex items-center justify-center">
+          <Image
+            src={"/illustrator/add-color.svg"}
+            alt=""
+            width={500}
+            height={100}
+          />
+        </div>
+
+        <div className="flex items-center justify-center">
+          <Image
+            src={"/illustrator/complete.svg"}
+            alt=""
+            width={500}
+            height={100}
+          />
+        </div>
+        <div className="flex flex-col justify-center gap-2">
+          <h1 className="text-3xl font-bold text-primary">
+            Time-Saving Efficiency
+          </h1>
+          <p className="text-muted-foreground">
+            We understand that your time is valuable, which is why we've
+            designed our platform to be incredibly efficient. With just a few
+            clicks, you can create a professional-looking portfolio that
+            accurately reflects your skills and experiences. Our pre-designed
+            templates and easy-to-use customization options allow you to focus
+            less on the technical details and more on what you want to showcase.
+            Say goodbye to spending hours on formatting and design-our tool does
+            the heavy lifting for you, so you can have your portfolio ready in
+            no time.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChooseUs;
