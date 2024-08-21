293e7e813514890fea5591dcc3a650e14810bca7
hix <canbedir100@gmail.com>
2024-08-21 20:18:23 +0300

    style: update stepbar with different colors for each step
    -Each step in the stepbar now has a distinct color, enhancing the visual feedback for users as they progress through the steps.

--- components/Create/StepBar.tsx
+++ components/Create/StepBar.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 
-const StepBar = ({step}:{step:number}) => {
+const StepBar = ({ step }: { step: number }) => {
   return (
-    <div className="w-full h-4 rounded-full bg-gray-200">
+    <div className="w-full h-5 rounded-full bg-gray-200">
       <div
-        className={`rounded-full h-4 transition-width duration-300 ${
-          step === 8 ? "bg-green-500 shadow-lg shadow-green-500" : "bg-primary"
-        }`}
+        className={`rounded-full h-5 transition-width duration-700 
+          ${step === 1 && `shadow-lg shadow-primary bg-primary`}
+          ${step === 2 && `shadow-lg shadow-blue-600 bg-blue-600`}
+          ${step === 3 && `shadow-lg shadow-orange-600 bg-orange-600`}
+          ${step === 4 && `shadow-lg shadow-yellow-500 bg-yellow-500`}
+          ${step === 5 && `shadow-lg shadow-cyan-500 bg-cyan-500`}
+          ${step === 6 && `shadow-lg shadow-pink-500 bg-pink-500`}
+          ${step === 7 && `shadow-lg shadow-rose-600 bg-rose-600`}
+          ${step === 8 && `shadow-lg shadow-green-600 bg-green-600`}
+          `}
         style={{
           width:
             step === 1
